519fdfd3eef6a9ee030ad22eadd955200bfffb20
Eduardo Felix <dudumacedofelix613@gmail.com>
2022-12-10 17:15:55 -0300

    configurando CORS

--- api/views/LoginView.py
+++ api/views/LoginView.py
@@ -2,7 +2,11 @@ from rest_framework import permissions, views, status
 from django.contrib.auth import login
 from rest_framework.response import Response
 from api.serializers.LoginSerializer import LoginSerializer
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
 
+csrf_protect_method = method_decorator(csrf_protect)
+@csrf_protect_method
 class Login(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
