ba82e0533e00c7154b060c2fd0f4c65293742a95
Eduardo Felix <dudumacedofelix613@gmail.com>
2022-12-10 17:37:05 -0300

    configurando CORS

--- backend/settings.py
+++ backend/settings.py
@@ -139,6 +139,13 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8000', 
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000', 
+    'http://127.0.0.1:3000', 
+    'http://127.0.0.1:8000', 
+    'http://localhost:8000', 
+    ]
+
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
